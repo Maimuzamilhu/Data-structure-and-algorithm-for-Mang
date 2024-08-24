2dc08bc6c7aed5e936368beb539f7141ca2f251b
Maimuzamilhu <muzamilkhalid808@gmail.com>
2024-08-24 08:14:44 +0500

    selection sort

---
+++ 04 Ternary, Bubble and Selection Sort/03 Selection sort.py
@@ -0,0 +1,19 @@
+
+def selectionsort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Min Index
+        min_inx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_inx]:
+                min_inx = j
+        ## Swap happend after every pass
+        arr[i], arr[min_inx] = arr[min_inx], arr[i]
+    return  arr
+
+
+
+
+arr = [70, 56, 23, 19, 25, 37, 48]
+result = selectionsort(arr)
+print("Array after using selection sort is:", result)
