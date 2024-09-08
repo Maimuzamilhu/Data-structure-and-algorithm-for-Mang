5182012d6203e97f9412f89d9d8c1f9359bdf814
Maimuzamilhu <muzamilkhalid808@gmail.com>
2024-09-08 18:39:11 +0500

    points are Colinear or not using slope and Area formula

--- 08 Interview Questions (FANG)/02 Checking Whether points are Colinear or not(Google).py
+++ 08 Interview Questions (FANG)/02 Checking Whether points are Colinear or not(Google).py
@@ -1,9 +1,10 @@
 # Using Slope formula
 def colinear(x1, x2, x3, y1, y2, y3):
-    # y2-y1/x2-x1 == y3-y2/x3-x2 if both slops are equal then it should be colinear
 
+    # y2-y1/x2-x1 == y3-y2/x3-x2 if both slops are equal then it should be colinear
+    # # area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) we can also calculate using area if area==0 then its colinear
     if ((y2-y1)*(x3-x1) == (y3-y2)*(x2-x1)):
-        print("Yes it's Linear")
+        print("Yes it's Co-Linear")
     else:
         print("its not")
 
